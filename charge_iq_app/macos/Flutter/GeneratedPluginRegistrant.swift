516cfd0924e934f20ce0911e58e4e2de9f68b20d
lithigesh <lithigesh@gmail.com>
2026-02-11 15:02:03 +0530

    chore: Update Firebase dependencies and fix plugin registration

--- charge_iq_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ charge_iq_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,7 @@ import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
